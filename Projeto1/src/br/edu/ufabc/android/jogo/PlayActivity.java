8302496e3d612a931c0c5328161931d68ea1de17
Timzzao <timzz1kk@gmail.com>
2015-08-13 04:00:03 -0300

    Last Version

--- Projeto1/src/br/edu/ufabc/android/jogo/PlayActivity.java
+++ Projeto1/src/br/edu/ufabc/android/jogo/PlayActivity.java
@@ -13,13 +13,16 @@ import android.widget.Toast;
 
 public class PlayActivity extends Activity {
 
+	public CountDownTimer wavetimer;
+
 	public final long[] minutos = { 0 };
 	public final long[] segundos = { 0 };
-	public final long[] novo = { 1000 };
+	public final long[] novo = { 0 };
+	public int pontuacao = 0;
 
 	public final int[] doces = new int[] { R.drawable.docinho_1,
 			R.drawable.docinho_2, R.drawable.docinho_3, R.drawable.docinho_4,
-			R.drawable.docinho_5 };
+			R.drawable.docinho_5, R.drawable.docinho_6 };
 	public int[] matrizDoce = new int[25];
 
 	public int flag = 25;
@@ -32,7 +35,7 @@ public class PlayActivity extends Activity {
 		setContentView(R.layout.activity_play);
 
 		for (int i = 0; i < matrizDoce.length; i++) {
-			matrizDoce[i] = doces[(int) (Math.random() * 5)];
+			matrizDoce[i] = doces[(int) (Math.random() * 6)];
 		}
 
 		atualizaGV(matrizDoce);
@@ -59,6 +62,7 @@ public class PlayActivity extends Activity {
 
 							atualizaGV(matrizDoce);
 							verificaMatchH(0);
+							verificaMatchV(0);
 
 							Toast.makeText(getBaseContext(), "Posicao 0",
 									Toast.LENGTH_SHORT).show();
@@ -71,6 +75,7 @@ public class PlayActivity extends Activity {
 
 							atualizaGV(matrizDoce);
 							verificaMatchH(0);
+							verificaMatchV(0);
 						}
 					} else if (flag == 24) {
 						if (flag == position + 1 || flag == position + 5) {
@@ -80,6 +85,7 @@ public class PlayActivity extends Activity {
 
 							atualizaGV(matrizDoce);
 							verificaMatchH(0);
+							verificaMatchV(0);
 						}
 					} else if (flag == 4) {
 						if (flag == position + 1 || flag == position - 5) {
@@ -89,6 +95,7 @@ public class PlayActivity extends Activity {
 
 							atualizaGV(matrizDoce);
 							verificaMatchH(0);
+							verificaMatchV(0);
 						}
 					} else if (flag == 1 || flag == 2 || flag == 3) {
 						if (flag == position + 1 || flag == position - 5
@@ -99,6 +106,7 @@ public class PlayActivity extends Activity {
 
 							atualizaGV(matrizDoce);
 							verificaMatchH(0);
+							verificaMatchV(0);
 						}
 					} else if (flag == 5 || flag == 10 || flag == 15) {
 						if (flag == position - 1 || flag == position - 5
@@ -109,6 +117,7 @@ public class PlayActivity extends Activity {
 
 							atualizaGV(matrizDoce);
 							verificaMatchH(0);
+							verificaMatchV(0);
 						}
 					} else if (flag == 9 || flag == 14 || flag == 19) {
 						if (flag == position + 1 || flag == position - 5
@@ -118,6 +127,7 @@ public class PlayActivity extends Activity {
 							matrizDoce[flag] = temp;
 							atualizaGV(matrizDoce);
 							verificaMatchH(0);
+							verificaMatchV(0);
 						}
 					} else if (flag == 21 || flag == 22 || flag == 23) {
 						if (flag == position + 1 || flag == position + 5
@@ -128,6 +138,7 @@ public class PlayActivity extends Activity {
 
 							atualizaGV(matrizDoce);
 							verificaMatchH(0);
+							verificaMatchV(0);
 						}
 					} else {
 						if (flag == position + 1 || flag == position + 5
@@ -138,6 +149,7 @@ public class PlayActivity extends Activity {
 
 							atualizaGV(matrizDoce);
 							verificaMatchH(0);
+							verificaMatchV(0);
 						}
 					}
 
@@ -151,24 +163,134 @@ public class PlayActivity extends Activity {
 			}
 		});
 
-		CountDownTimer wavetimer = new myTimer(novo[0] + 3000, 1000).start();
+		wavetimer = new myTimer(novo[0] + 120000, 1000).start();
+
 	}
 
 	public void verificaMatchH(int i) {
-		if (matrizDoce[i] == matrizDoce[i + 1]
-				&& matrizDoce[i + 1] == matrizDoce[i + 2]) {
-			matrizDoce[i] = doces[(int) (Math.random() * 5)];
-			matrizDoce[i + 1] = doces[(int) (Math.random() * 5)];
-			matrizDoce[i + 2] = doces[(int) (Math.random() * 5)];
+
+		if (i == 0 || i == 1 || i == 2 || i == 5 || i == 6 || i == 7 || i == 10
+				|| i == 11 || i == 12 || i == 15 || i == 16 || i == 17
+				|| i == 20 || i == 21 || i == 22) {
+			if (matrizDoce[i] == matrizDoce[i + 1]
+					&& matrizDoce[i + 1] == matrizDoce[i + 2]) {
+				matrizDoce[i] = doces[(int) (Math.random() * 6)];
+				matrizDoce[i + 1] = doces[(int) (Math.random() * 6)];
+				matrizDoce[i + 2] = doces[(int) (Math.random() * 6)];
+
+
+
+				pontuacao += 1;
+				atualizaPontuacao();
+
+				if ((matrizDoce[i + 2] == matrizDoce[i + 3])
+						&& (i == 0 || i == 1 || i == 5 || i == 6 || i == 10
+								|| i == 11 || i == 15 || i == 16 || i == 20 || i == 21)) {
+					matrizDoce[i] = doces[(int) (Math.random() * 6)];
+					matrizDoce[i + 1] = doces[(int) (Math.random() * 6)];
+					matrizDoce[i + 2] = doces[(int) (Math.random() * 6)];
+					matrizDoce[i + 3] = doces[(int) (Math.random() * 6)];
+
+
+
+					pontuacao += 5;
+					atualizaPontuacao();
+
+					if ((matrizDoce[i + 3] == matrizDoce[i + 4])
+							&& (i == 0 || i == 5 || i == 10 || i == 15 || i == 20)) {
+
+						matrizDoce[i] = doces[(int) (Math.random() * 6)];
+						matrizDoce[i + 1] = doces[(int) (Math.random() * 6)];
+						matrizDoce[i + 2] = doces[(int) (Math.random() * 6)];
+						matrizDoce[i + 3] = doces[(int) (Math.random() * 6)];
+						matrizDoce[i + 4] = doces[(int) (Math.random() * 6)];
+
+
+
+						pontuacao += 15;
+						atualizaPontuacao();
+
+					}
+				}
+			}
+			atualizaPontuacao();
+
+			wavetimer.cancel();
+
+			wavetimer = new myTimer(novo[0], 1000).start();
 
 			atualizaGV(matrizDoce);
+
+
 		}
 
-		if (i == 25) {
+		if (i == 23) {
 			return;
 		}
 
 		verificaMatchH(i + 1);
+	}
+
+	public void verificaMatchV(int i) {
+		if (i == 0 || i == 1 || i == 2 || i == 3 || i == 4 || i == 5 || i == 6
+				|| i == 7 || i == 8 || i == 9 || i == 10 || i == 11 || i == 12
+				|| i == 13 || i == 14) {
+			if (matrizDoce[i] == matrizDoce[i + 5]
+					&& matrizDoce[i + 5] == matrizDoce[i + 10]) {
+				matrizDoce[i] = doces[(int) (Math.random() * 6)];
+				matrizDoce[i + 5] = doces[(int) (Math.random() * 6)];
+				matrizDoce[i + 10] = doces[(int) (Math.random() * 6)];
+
+
+
+				pontuacao += 1;
+				atualizaPontuacao();
+
+				if ((matrizDoce[i + 10] == matrizDoce[i + 15])
+						&& (i == 0 || i == 1 || i == 2 || i == 3 || i == 4
+								|| i == 5 || i == 6 || i == 7 || i == 8 || i == 9)) {
+					matrizDoce[i] = doces[(int) (Math.random() * 6)];
+					matrizDoce[i + 5] = doces[(int) (Math.random() * 6)];
+					matrizDoce[i + 10] = doces[(int) (Math.random() * 6)];
+					matrizDoce[i + 15] = doces[(int) (Math.random() * 6)];
+
+
+
+					pontuacao += 5;
+					atualizaPontuacao();
+
+					if ((matrizDoce[i + 15] == matrizDoce[i + 20])
+							&& (i == 0 || i == 1 || i == 2 || i == 3 || i == 4)) {
+
+						matrizDoce[i] = doces[(int) (Math.random() * 6)];
+						matrizDoce[i + 5] = doces[(int) (Math.random() * 6)];
+						matrizDoce[i + 10] = doces[(int) (Math.random() * 6)];
+						matrizDoce[i + 15] = doces[(int) (Math.random() * 6)];
+						matrizDoce[i + 20] = doces[(int) (Math.random() * 6)];
+
+
+
+						pontuacao += 15;
+						atualizaPontuacao();
+
+					}
+				}
+			}
+
+			wavetimer.cancel();
+
+			wavetimer = new myTimer(novo[0], 1000).start();
+
+			atualizaGV(matrizDoce);
+
+
+		}
+
+		if (i == 15) {
+			return;
+		}
+
+		verificaMatchV(i + 1);
 	}
 
 	public void atualizaGV(int[] matrizDoce) {
@@ -194,6 +316,12 @@ public class PlayActivity extends Activity {
 		txtTempo.setText(txtMin + ":" + txtSeg);
 	}
 
+	public void atualizaPontuacao() {
+		TextView txtPonto = (TextView) findViewById(R.id.textView1);
+
+		txtPonto.setText(Integer.toString(pontuacao));
+	}
+
 	public class myTimer extends CountDownTimer {
 
 		private long millisActual;
@@ -205,12 +333,10 @@ public class PlayActivity extends Activity {
 
 		@Override
 		public void onTick(long millisUntilFinished) {
-			Log.d("onTick", "Novo = " + novo[0]);
 			novo[0] = millisUntilFinished;
 			minutos[0] = millisUntilFinished / 60000;
 			segundos[0] = ((millisUntilFinished % 60000) - (millisUntilFinished % 1000)) / 1000;
 			atualizaTexto(minutos[0], segundos[0]);
-			Log.d("onTick", "Entrou");
 		}
 
 		@Override
